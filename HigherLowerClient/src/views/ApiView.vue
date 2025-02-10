7fe205197927fe0a92725652fe7532b8f69e253c
Tommy Mai <tommymaimai@gmail.com>
2025-02-10 17:09:05 +0100

    fixed price in the data model

--- HigherLowerClient/src/views/ApiView.vue
+++ HigherLowerClient/src/views/ApiView.vue
@@ -5,20 +5,21 @@
   <div v-else>
       <h1 style="align-content: center; justify-content: center; margin-bottom:20%; position:fixed; top: 20%">Guess what game is more expensive!</h1>
       <div style="display: grid; grid-template-columns: repeat(2, 1fr); grid-gap: 200px;">
-      <div @click="showPriceAfterGuess" style="height:30%">
-        <h2>{{games[0]['gamedata'][firstCounter]['name']}}</h2>
+      <div @click="showPriceAfterGuess">
+        <h2 style="overflow: hidden">{{games[0]['gamedata'][firstCounter]['name']}}</h2>
 <!--        <h5>{{games[0]['gamedata'][firstCounter]['date']}}</h5>-->
         <img :src="games[0]['gamedata'][firstCounter]['image']" alt="Source is corrupt, pretend theres a fine picture here =)"/>
         <h2>The price is: {{games[0]['gamedata'][firstCounter]['price']}}</h2>
       </div>
-      <div @click="showPriceAfterGuess" style="height:30%">
-        <h2>{{games[0]['gamedata'][secondCounter]['name']}}</h2>
+      <div @click="calculatePriceAfterFirstGameClicked">
+        <h2 style="overflow: hidden">{{games[0]['gamedata'][secondCounter]['name']}}</h2>
 <!--        <h5>{{games[0]['gamedata'][secondCounter]['date']}}</h5>-->
         <img :src="games[0]['gamedata'][secondCounter]['image']" alt="Source is corrupt, pretend theres a fine picture here =)"/>
         <h2>{{ showPrice ? "The price is: " + games[0]['gamedata'][secondCounter]['price'] : ""}}</h2>
       </div>
 
-        <h1>The most expensive game was: {{showPrice ?  games[0]['gamedata'][isFirstGameMoreExpensive ? firstCounter : secondCounter]['price'] : ""}}</h1>
+        <h1>The most expensive game was: {{showPrice ? games[0]['gamedata'][isFirstGameMoreExpensive ? firstCounter : secondCounter]['price'] : "" || isDraw ? "Both are the same price you get a free pass" : ""}}</h1>
+        <h1>Score: {{score}}</h1>
     </div>
   </div>
 
@@ -38,21 +39,41 @@ setup(){
       secondCounter : ref(1),
       showPrice: ref(false),
       isFirstGameMoreExpensive : ref(false),
+      isDraw : ref(false),
+      clickedFirstGame: ref(false),
+      score : ref(0),
     }
   },
   methods: {
-  calculatePrice(){
-    this.isFirstGameMoreExpensive = false;
-    let data =  this.games[0]['gamedata'];
-    this.isFirstGameMoreExpensive = parseFloat(data[this.firstCounter]['price'].split(",")[0]) > parseFloat(data[this.secondCounter]['price'].split(" ")[0]);
+  calculateScore(){
+    if(this.clickedFirstGame && this.isFirstGameMoreExpensive){
+      this.score++;
+    }
+    if(!this.clickedFirstGame && !this.isFirstGameMoreExpensive){
+      this.score++;
+    }
   },
+  calculatePriceFirstGameClicked(){
+    this.isFirstGameMoreExpensive = false;
+    this.clickedFirstGame = true;
+    let data = this.games[0]['gamedata'];
+    this.isFirstGameMoreExpensive = parseFloat(data[this.firstCounter]['price'].split(",")[0]) > parseFloat(data[this.secondCounter]['price'].split(" ")[0]);
+    this.isDraw = parseFloat(data[this.firstCounter]['price'].split(",")[0]) === parseFloat(data[this.secondCounter]['price'].split(" ")[0]);
+  },
+    calculatePriceSecondGameClicked(){
+    this.clickedFirstGame = false;
+    },
     showPriceAfterGuess(){
       this.showPrice = true;
-      this.calculatePrice();
+      this.calculatePriceFirstGameClicked();
+      this.calculateScore();
       setTimeout(()=>{
         this.incrementCounters();
         this.showPrice = false;
-        }, 1500);
+        this.isDraw = false;
+        this.clickedFirstGame = true;
+
+      }, 1500);
 
     } ,
     incrementCounters(){
@@ -60,21 +81,26 @@ setup(){
       this.secondCounter += 2;
     }
   },
-  // randomizeArray(inputArray){
-  //   for(let i = 0; i < inputArray.length; i++){
-  //     let randomIndex = Math.floor(Math.random() * inputArray.length);
-  //     [inputArray[i], inputArray[randomIndex]] = [inputArray[randomIndex], inputArray[i]]
-  //   }
-  //   return inputArray;
-  // },
+
   async beforeMount() {
     await fetch('http://localhost:3000/GetGamesInfo')
       .then(res => res.json())
       .then(data => this.games = data)
+      .then(data => console.log(data))
       .catch(err => console.log(err))
+    randomizeArray(this.games[0]['gamedata']);
   },
   mounted() {
   }
 }
+
+function  randomizeArray(inputArray){
+  for(let i = 0; i < inputArray.length; i++){
+    let randomIndex = Math.floor(Math.random() * inputArray.length);
+    [inputArray[i], inputArray[randomIndex]] = [inputArray[randomIndex], inputArray[i]]
+  }
+  return inputArray;
+}
+
 
 </script>
