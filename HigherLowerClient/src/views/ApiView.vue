54b38d8e99e0e66a01b064f6999c3e19a07f276d
Tommy Mai <tommymaimai@gmail.com>
2025-02-12 17:04:55 +0100

    Added highscore from database, highscore updates if score goes higher than highscore.

--- HigherLowerClient/src/views/ApiView.vue
+++ HigherLowerClient/src/views/ApiView.vue
@@ -3,10 +3,10 @@
     <h2>Loading...</h2>
   </div>
   <div v-else>
-      <h1 style="align-content: center; justify-content: center; margin-bottom:20%; position:fixed; top: 20%">Guess what game is more expensive!</h1>
-      <div style="display: grid; grid-template-columns: repeat(2, 1fr); grid-gap: 200px;">
+      <h1 style="align-content: center; justify-content: center;position:fixed; top: 20%">Guess what game is more expensive! The highscore is: {{highscore}}</h1>
+    <div style="display: grid; grid-template-columns: repeat(2, 1fr); grid-gap: 200px; margin-top:10%">
       <div @click="calculatePriceAfterGameClick(true,false)">
-        <h2 style="overflow: hidden">{{games[0]['gamedata'][firstCounter]['name']}}</h2>
+        <h2>{{games[0]['gamedata'][firstCounter]['name']}}</h2>
 <!--        <h5>{{games[0]['gamedata'][firstCounter]['date']}}</h5>-->
         <img :src="games[0]['gamedata'][firstCounter]['image']" alt="Source is corrupt, pretend theres a fine picture here =)"/>
         <h2>The price is: {{games[0]['gamedata'][firstCounter]['price']}}</h2>
@@ -19,7 +19,8 @@
       </div>
 
         <h1>The most expensive game was: {{showPrice ? games[0]['gamedata'][isFirstGameMoreExpensive ? firstCounter : secondCounter]['price'] : "" || isDraw ? "Both are the same price you get a free pass" : ""}}</h1>
-        <h1>Score: {{score}}</h1>
+        <h2>{{ this.Message }}</h2>
+        <h1>Your score: {{score}}</h1>
     </div>
   </div>
 
@@ -34,6 +35,7 @@ setup(){
 },
   data () {
     return {
+      highscore : 1,
       games: [],
       firstCounter : ref(0),
       secondCounter : ref(1),
@@ -43,25 +45,39 @@ setup(){
       score : ref(0),
       clickedFirstGame: ref(false),
       clickedSecondGame: ref(false),
+      Message: ref(""),
+      isHighscoreLowerThanCurrentScore: ref(false)
     }
   },
   methods: {
   calculateScore(){
     if(this.clickedFirstGame && this.isFirstGameMoreExpensive){
       this.score++;
+      if(this.highscore < this.score) {this.highscore = this.score;}
+      return this.Message = "Correct +1 score!";
+    }
+    if(this.isDraw === true){
+      return this.Message = "Both games cost the same, you get a free pass"
     }
     if(this.clickedSecondGame && !this.isFirstGameMoreExpensive){
       this.score++;
+      if(this.highscore < this.score) {this.highscore = this.score;}
+      return this.Message = "Correct +1 score!";
     }
+    this.Message = "You lost, better luck next time!";
+    pushHighscoreAfterLoss(this.score);
+    setTimeout(()=>{
+      location.reload();
+    }, 2000)
+
   },
   calculatePriceAfterGameClick(isFirstClicked, isSecondClicked){
     this.isFirstGameMoreExpensive = false;
     this.clickedFirstGame = isFirstClicked;
     this.clickedSecondGame = isSecondClicked
     let data = this.games[0]['gamedata'];
-    this.isFirstGameMoreExpensive = parseFloat(data[this.firstCounter]['price'].split(",")[0]) > parseFloat(data[this.secondCounter]['price'].split(" ")[0]);
-    this.isDraw = parseFloat(data[this.firstCounter]['price'].split(",")[0]) === parseFloat(data[this.secondCounter]['price'].split(" ")[0]);
-
+    this.isFirstGameMoreExpensive = parseFloat(data[this.firstCounter]['price'].replace('.', "").split(",")[0]) > parseFloat(data[this.secondCounter]['price'].replace('.', "").split(" ")[0]);
+    this.isDraw = parseFloat(data[this.firstCounter]['price'].replace('.', "").split(" ")[0]) === parseFloat(data[this.secondCounter]['price'].replace('.', "").split(" ")[0]);
     this.showPriceAfterGuess()
   },
     showPriceAfterGuess(){
@@ -72,9 +88,9 @@ setup(){
         this.showPrice = false;
         this.isDraw = false;
         this.clickedFirstGame = false;
-        this.clickedSecondGame = false;
-
-      }, 3000);
+        this.clickedSecondGame = false
+        this.Message = "";
+      }, 2500);
 
     } ,
     incrementCounters(){
@@ -83,16 +99,29 @@ setup(){
     }
   },
 
+
   async beforeMount() {
+    await fetch('http://localhost:3000/GetHighScore')
+     .then(res => res.json())
+     .then(data => this.highscore = data[0]['highscore']);
     await fetch('http://localhost:3000/GetGamesInfo')
       .then(res => res.json())
       .then(data => this.games = data)
-      .then(data => console.log(data))
       .catch(err => console.log(err))
     randomizeArray(this.games[0]['gamedata']);
   },
   mounted() {
   }
+}
+
+async function pushHighscoreAfterLoss(highscore){
+  await fetch('http://localhost:3000/UpdateHighscore', {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "PUT",
+    body: JSON.stringify({highscore: highscore})
+  })
 }
 
 function  randomizeArray(inputArray){
