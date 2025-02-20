6658d7769acc3957b4b02976c1269d151195d29d
Tommy Mai <tommymaimai@gmail.com>
2025-02-20 17:14:42 +0100

    refactored highscore

---
+++ HigherLowerClient/src/components/HighScore.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 style="align-content: center; justify-content: center;position:fixed; top: 20%">Guess what game is more expensive! The currents highscore is: {{highscore}}</h1>
+</template>
+<script>
+export default {
+  props: {
+    highscore: String
+  }
+}
+</script>
