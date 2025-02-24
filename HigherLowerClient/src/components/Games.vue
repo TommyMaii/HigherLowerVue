32fbbb8c99b6643309c9796e968bc960adc290d9
Tommy Mai <tommymaimai@gmail.com>
2025-02-24 11:49:43 +0100

    Refactored and split games code

--- HigherLowerClient/src/components/Games.vue
+++ HigherLowerClient/src/components/Games.vue
@@ -1,127 +1,82 @@
 <script>
-import {ref} from "vue";
+import { defineProps } from 'vue';
+import HighScore from "@/components/HighScore.vue";
+import MessageComponent from "@/components/MessageComponent.vue";
+import CurrentScore from "@/components/CurrentScore.vue";
+import GameLogic from '@/components/GameLogic.js';
 
 export default {
-  setup(){
-  },
-  data () {
+  components: { HighScore, MessageComponent, CurrentScore },
+  props: { games: Array },
+  setup(props) {
+    const {
+      highscore,
+      score,
+      firstCounter,
+      secondCounter,
+      showPrice,
+      message,
+      calculatePriceAfterGameClick
+    } = GameLogic(props.games);
 
     return {
-      highscore : ref(0),
-      databaseHighscore : ref(0),
-      firstCounter : ref(0),
-      secondCounter : ref(1),
-      showPrice: ref(false),
-      isFirstGameMoreExpensive : ref(false),
-      isDraw : ref(false),
-      score : ref(0),
-      clickedFirstGame: ref(false),
-      clickedSecondGame: ref(false),
-      Message: ref(""),
-      isHighscoreLowerThanCurrentScore: ref(false)
-    }
-  },
-  props:{
-    games : Array,
-  },
-  methods: {
-
-    calculateScore() {
-      if (this.isDraw) {
-        this.Message = "Both games cost the same, you get a free pass";
-        return;
-      }
-
-      const isCorrect =
-        (this.clickedFirstGame && this.isFirstGameMoreExpensive) ||
-        (this.clickedSecondGame && !this.isFirstGameMoreExpensive);
-
-      if (isCorrect) {
-        this.score++;
-        this.highscore = Math.max(this.highscore, this.score);
-        this.Message = "Correct +1 score!";
-      } else {
-        this.Message = "You lost, better luck next time!";
-        if (this.highscore > this.databaseHighscore) {
-          pushHighscoreAfterLoss(this.highscore);
-          this.Message = "You lost, but you got a new high score!";
-        }
-        setTimeout(() => {
-          location.reload();
-        }, 2000);
-      }
-    },
-
-    calculatePriceAfterGameClick(isFirstClicked, isSecondClicked){
-      this.isFirstGameMoreExpensive = false;
-      this.clickedFirstGame = isFirstClicked;
-      this.clickedSecondGame = isSecondClicked
-      let data = this.games[0]['gamedata'];
-      this.isFirstGameMoreExpensive = parseFloat(data[this.firstCounter]['price'].replace('.', "").split(",")[0]) > parseFloat(data[this.secondCounter]['price'].replace('.', "").split(" ")[0]);
-      this.isDraw = parseFloat(data[this.firstCounter]['price'].replace('.', "").split(" ")[0]) === parseFloat(data[this.secondCounter]['price'].replace('.', "").split(" ")[0]);
-      this.showPriceAfterGuess()
-    },
-
-    showPriceAfterGuess(){
-      this.showPrice = true;
-      this.calculateScore();
-      setTimeout(()=>{
-        this.incrementCounters();
-        this.resetVariables();
-      }, 2000);
-
-    } ,
-    incrementCounters(){
-      this.firstCounter += 2;
-      this.secondCounter += 2;
-    },
-
-    resetVariables(){
-      Object.assign(this, {
-        showPrice: false,
-        isDraw: false,
-        clickedFirstGame: false,
-        clickedSecondGame: false,
-        Message: "",
-      });
-    },
-  },
-
-  async beforeMount() {
-    await fetch('http://localhost:3000/GetHighScore')
-      .then(res => res.json())
-      .then(data => this.highscore= data[0]['highscore'])
-      .then(data => this.databaseHighscore = data);
-  },
-  mounted() {
+      highscore,
+      score,
+      firstCounter,
+      secondCounter,
+      showPrice,
+      message,
+      calculatePriceAfterGameClick
+    };
   }
-}
-
-async function pushHighscoreAfterLoss(highscore){
-  await fetch('http://localhost:3000/UpdateHighscore', {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method: "PUT",
-    body: JSON.stringify({highscore: highscore})
-  })
-}
-
+};
 </script>
 
 <template>
-  <div @click="calculatePriceAfterGameClick(true,false)">
-    <h2>{{games[0]['gamedata'][firstCounter]['name']}}</h2>
-    <img :src="games[0]['gamedata'][firstCounter]['image']" alt="Source is corrupt, pretend theres a fine picture here =)"/>
-    <h2>The price is: {{games[0]['gamedata'][firstCounter]['price']}}</h2>
-  </div>
-  <div @click="calculatePriceAfterGameClick(false,true)">
-    <h2 style="overflow: hidden">{{games[0]['gamedata'][secondCounter]['name']}}</h2>
-    <img :src="games[0]['gamedata'][secondCounter]['image']" alt="Source is corrupt, pretend theres a fine picture here =)"/>
-    <h2>{{ showPrice ? "The price is: " + games[0]['gamedata'][secondCounter]['price'] : ""}}</h2>
+  <div class="games-container">
+    <div class="game-card" @click="calculatePriceAfterGameClick(true, false)">
+      <h2>{{ games[0]?.gamedata[firstCounter]?.name }}</h2>
+      <img :src="games[0]?.gamedata[firstCounter]?.image" alt="Game Image"/>
+      <h2>The price is: {{ games[0]?.gamedata[firstCounter]?.price }}</h2>
+    </div>
+    <div class="game-card" @click="calculatePriceAfterGameClick(false, true)">
+      <h2>{{ games[0]?.gamedata[secondCounter]?.name }}</h2>
+      <img :src="games[0]?.gamedata[secondCounter]?.image" alt="Game Image"/>
+      <h2 v-if="showPrice">The price is: {{ games[0]?.gamedata[secondCounter]?.price }}</h2>
+    </div>
+    <HighScore :highscore="highscore"/>
+    <MessageComponent :message="message"/>
+    <CurrentScore :score="score"/>
   </div>
 </template>
 
 <style scoped>
+.games-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 200px;
+}
 
+.game-card {
+  width: 500px;
+  height: 330px;
+  background: #1e1e1e;
+  padding: 15px;
+  border-radius: 10px;
+  text-align: center;
+  cursor: pointer;
+  transition: transform 0.2s, box-shadow 0.3s;
+}
+
+.game-card:hover {
+  transform: scale(1.05);
+  box-shadow: 0px 4px 10px rgba(0, 255, 153, 0.5);
+}
+
+.game-card h2 {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
+}
 </style>
