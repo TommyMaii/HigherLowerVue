f2cf6f50e54b57e2559f3b10e62788bc937be477
Tommy Mai <tommymaimai@gmail.com>
2025-02-26 17:17:44 +0100

    Changed gamelogic to support 2 different game types.

--- HigherLowerClient/src/components/GameLogic.js
+++ HigherLowerClient/src/components/GameLogic.js
@@ -1,14 +1,13 @@
 import { ref, watch, onBeforeMount } from 'vue';
 
-export default function GameLogic(gamesProp) {
-  const games = ref(gamesProp); // Make games reactive
-
+export default function GameLogic(gamesProp, gameMode = 'price') {
+  const games = ref(gamesProp);
   const highscore = ref(0);
   const databaseHighscore = ref(0);
   const firstCounter = ref(0);
   const secondCounter = ref(1);
-  const showPrice = ref(false);
-  const isFirstGameMoreExpensive = ref(false);
+  const showResult = ref(false);
+  const isFirstGameBetter = ref(false);
   const isDraw = ref(false);
   const score = ref(0);
   const clickedFirstGame = ref(false);
@@ -22,23 +21,27 @@ export default function GameLogic(gamesProp) {
   }, { immediate: true });
 
   const calculateScore = () => {
-    if (clickedFirstGame.value && isFirstGameMoreExpensive.value) {
+    if (clickedFirstGame.value && isFirstGameBetter.value) {
       score.value++;
     } else if (isDraw.value) {
-      message.value = "Both games cost the same, you get a free pass!";
+      message.value = "It's a tie! You get a free pass!";
+      console.log(message.value)
+
       return;
-    } else if (clickedSecondGame.value && !isFirstGameMoreExpensive.value) {
+    } else if (clickedSecondGame.value && !isFirstGameBetter.value) {
       score.value++;
     } else {
-      message.value = "You lost, better luck next time!";
+      message.value = "You lost! Better luck next time!";
+      console.log(message.value)
 
       if (score.value > databaseHighscore.value) {
         highscore.value = score.value;
         message.value = "You lost, but you got a new high score!";
         pushHighscoreAfterLoss(highscore.value);
+        console.log(message.value)
       }
 
-      setTimeout(() => location.reload(), 3000);
+      setTimeout(() => location.reload(), 2000);
       return;
     }
 
@@ -47,41 +50,52 @@ export default function GameLogic(gamesProp) {
     }
 
     message.value = "Correct! +1 Score!";
+    console.log(message.value)
+
   };
 
-  const calculatePriceAfterGameClick = (isFirstClicked, isSecondClicked) => {
+  const calculateComparison = (game1, game2) => {
+    if (!game1 || !game2) return;
+
+    let value1, value2;
+
+    if (gameMode === 'price') {
+      value1 = parseFloat(game1.price.replace('.', "").split(",")[0]);
+      value2 = parseFloat(game2.price.replace('.', "").split(" ")[0]);
+    } else if (gameMode === 'reviews') {
+      value1 = parseInt(game1.reviews, 10);
+      value2 = parseInt(game2.reviews, 10);
+    }
+
+    isFirstGameBetter.value = value1 > value2;
+    isDraw.value = value1 === value2;
+  };
+
+  const calculateGameAfterClick = (isFirstClicked, isSecondClicked) => {
     if (!games.value || games.value.length === 0 || !games.value[0]?.gamedata) {
       console.error("Error: Games data is not available yet!");
       return;
     }
 
-    isFirstGameMoreExpensive.value = false;
     clickedFirstGame.value = isFirstClicked;
     clickedSecondGame.value = isSecondClicked;
 
     const data = games.value[0].gamedata;
 
     if (data[firstCounter.value] && data[secondCounter.value]) {
-      isFirstGameMoreExpensive.value =
-        parseFloat(data[firstCounter.value].price.replace('.', "").split(",")[0]) >
-        parseFloat(data[secondCounter.value].price.replace('.', "").split(" ")[0]);
-
-      isDraw.value =
-        parseFloat(data[firstCounter.value].price.replace('.', "").split(" ")[0]) ===
-        parseFloat(data[secondCounter.value].price.replace('.', "").split(" ")[0]);
-
-      showPriceAfterGuess();
+      calculateComparison(data[firstCounter.value], data[secondCounter.value]);
+      showResultAfterGuess();
     } else {
       console.warn("Invalid index access: firstCounter or secondCounter is out of range.");
     }
   };
 
-  const showPriceAfterGuess = () => {
-    showPrice.value = true;
+  const showResultAfterGuess = () => {
+    showResult.value = true;
     calculateScore();
     setTimeout(() => {
       incrementCounters();
-      showPrice.value = false;
+      showResult.value = false;
       isDraw.value = false;
       clickedFirstGame.value = false;
       clickedSecondGame.value = false;
@@ -101,7 +115,7 @@ export default function GameLogic(gamesProp) {
         method: "PUT",
         body: JSON.stringify({ highscore: newHighscore })
       });
-      const result = await response.json();
+
       if (!response.ok) {
         console.error("Failed to update high score on server.");
       } else {
@@ -135,162 +149,10 @@ export default function GameLogic(gamesProp) {
     score,
     firstCounter,
     secondCounter,
-    showPrice,
+    showResult,
     message,
-    calculatePriceAfterGameClick,
+    calculateComparison,
+    calculateGameAfterClick,
     calculateScore,
   };
 }
-
-// export default function GameLogic(gamesProp, gameMode = 'price') {
-//   const games = ref(gamesProp); // Make games reactive
-//   const highscore = ref(0);
-//   const databaseHighscore = ref(0);
-//   const firstCounter = ref(0);
-//   const secondCounter = ref(1);
-//   const showResult = ref(false);
-//   const isFirstGameBetter = ref(false);
-//   const isDraw = ref(false);
-//   const score = ref(0);
-//   const clickedFirstGame = ref(false);
-//   const clickedSecondGame = ref(false);
-//   const message = ref("");
-//
-//   watch(gamesProp, (newGames) => {
-//     if (newGames && newGames.length > 0) {
-//       games.value = newGames;
-//     }
-//   }, { immediate: true });
-//
-//   const calculateScore = () => {
-//     if (clickedFirstGame.value && isFirstGameBetter.value) {
-//       score.value++;
-//     } else if (isDraw.value) {
-//       message.value = "It's a tie! You get a free pass!";
-//       return;
-//     } else if (clickedSecondGame.value && !isFirstGameBetter.value) {
-//       score.value++;
-//     } else {
-//       message.value = "You lost! Better luck next time!";
-//
-//       if (score.value > databaseHighscore.value) {
-//         highscore.value = score.value;
-//         message.value = "You lost, but you got a new high score!";
-//         pushHighscoreAfterLoss(highscore.value);
-//       }
-//
-//       setTimeout(() => location.reload(), 3000);
-//       return;
-//     }
-//
-//     if (score.value > highscore.value) {
-//       highscore.value = score.value;
-//     }
-//
-//     message.value = "Correct! +1 Score!";
-//   };
-//
-//   const calculateComparison = (game1, game2) => {
-//     if (!game1 || !game2) return;
-//
-//     let value1, value2;
-//
-//     if (gameMode === 'price') {
-//       value1 = parseFloat(game1.price.replace('.', "").split(",")[0]);
-//       value2 = parseFloat(game2.price.replace('.', "").split(" ")[0]);
-//     } else if (gameMode === 'reviews') {
-//       value1 = parseInt(game1.reviews, 10);
-//       value2 = parseInt(game2.reviews, 10);
-//     }
-//
-//     isFirstGameBetter.value = value1 > value2;
-//     isDraw.value = value1 === value2;
-//   };
-//
-//   const calculateGameAfterClick = (isFirstClicked, isSecondClicked) => {
-//     if (!games.value || games.value.length === 0 || !games.value[0]?.gamedata) {
-//       console.error("Error: Games data is not available yet!");
-//       return;
-//     }
-//
-//     clickedFirstGame.value = isFirstClicked;
-//     clickedSecondGame.value = isSecondClicked;
-//
-//     const data = games.value[0].gamedata;
-//
-//     if (data[firstCounter.value] && data[secondCounter.value]) {
-//       calculateComparison(data[firstCounter.value], data[secondCounter.value]);
-//       showResultAfterGuess();
-//     } else {
-//       console.warn("Invalid index access: firstCounter or secondCounter is out of range.");
-//     }
-//   };
-//
-//   const showResultAfterGuess = () => {
-//     showResult.value = true;
-//     calculateScore();
-//     setTimeout(() => {
-//       incrementCounters();
-//       showResult.value = false;
-//       isDraw.value = false;
-//       clickedFirstGame.value = false;
-//       clickedSecondGame.value = false;
-//       message.value = "";
-//     }, 2000);
-//   };
-//
-//   const incrementCounters = () => {
-//     firstCounter.value += 2;
-//     secondCounter.value += 2;
-//   };
-//
-//   const pushHighscoreAfterLoss = async (newHighscore) => {
-//     try {
-//       const response = await fetch('http://localhost:3000/UpdateHighscore', {
-//         headers: { "Content-Type": "application/json" },
-//         method: "PUT",
-//         body: JSON.stringify({ highscore: newHighscore })
-//       });
-//
-//       if (!response.ok) {
-//         console.error("Failed to update high score on server.");
-//       } else {
-//         console.log("High score updated successfully!");
-//       }
-//     } catch (error) {
-//       console.error("Error updating high score:", error);
-//     }
-//   };
-//
-//   onBeforeMount(async () => {
-//     try {
-//       const response = await fetch('http://localhost:3000/GetHighScore');
-//       const data = await response.json();
-//
-//       if (data.length > 0 && data[0]?.highscore !== undefined) {
-//         highscore.value = data[0].highscore;
-//         databaseHighscore.value = data[0].highscore;
-//       } else {
-//         console.warn("No high score data found, initializing to 0.");
-//         highscore.value = 0;
-//         databaseHighscore.value = 0;
-//       }
-//     } catch (error) {
-//       console.error("Error fetching high score:", error);
-//     }
-//   });
-//
-//   return {
-//     highscore,
-//     score,
-//     firstCounter,
-//     secondCounter,
-//     showResult,
-//     message,
-//     calculateGameAfterClick,
-//     calculateScore,
-//   };
-// }
-// const gameLogic = GameLogic(gamesProp, 'price');
-// const gameLogic = GameLogic(gamesProp, 'reviews');
-
